a6927e88c3baab7959c2d634d719d84494f5cc1a
maher malhem <36564872+mahermalhem@users.noreply.github.com>
2020-12-25 15:32:23 +0200

    Constant

---
+++ Constant/API.dart
@@ -0,0 +1,25 @@
+
+const loginByUserNameAndPass='https://apex.oracle.com/pls/apex/maherdb/USER/getUserByUNandPW';
+
+const getAllMoviesApi='https://apex.oracle.com/pls/apex/maherdb/MOVIES/getAllMovie';
+
+const getMovieDetailById='https://apex.oracle.com/pls/apex/maherdb/MOVIES/getMovieById/';
+
+const getMovieCommentsById ='https://apex.oracle.com/pls/apex/maherdb/MOVIES/getCommentsByMovieId/';
+
+const postPredictionFormApi='https://apex.oracle.com/pls/apex/maherdb/USER/createNewPredictForm';
+
+const getMoviesByType='https://apex.oracle.com/pls/apex/maherdb/MOVIES/getMovieByType';
+
+const postMovieCommentByUserIdAndMovieId='https://apex.oracle.com/pls/apex/maherdb/MOVIES/createCommentForMovie';
+
+const postNewUserAccount='https://apex.oracle.com/pls/apex/maherdb/USER/createNewUser';
+
+const getUserFormsByUserId='https://apex.oracle.com/pls/apex/maherdb/MOVIES/getFormByUserId';
+
+const postRatingMovieApi='https://apex.oracle.com/pls/apex/maherdb/MOVIES/vote';
+
+const postHelpSurvey='https://apex.oracle.com/pls/apex/maherdb/USER/helpSurvey';
+
+
+
---
+++ Constant/Values.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final Color textInputColor=Color(0xff4d0000);
+
+final Color floatingColor =Color(0xff4d0000);
+
+
+
+LinearGradient defaultlinearGradiantDecorantion=LinearGradient(
+	begin: Alignment.topLeft,
+	end: Alignment.bottomCenter,
+	colors: [
+		Colors.redAccent,
+		Colors.black54,
+		Colors.lightBlue
+	],
+);
